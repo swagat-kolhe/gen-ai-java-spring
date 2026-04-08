9ca18845d8de9f6f981da877a266c349329e2cb2
Swagat <swagatkolhe14@gmail.com>
2026-04-08 17:33:14 -0500

    Google Vertex AI integration

--- src/main/java/com/genai/java/spring/config/AIProviderConfig.java
+++ src/main/java/com/genai/java/spring/config/AIProviderConfig.java
@@ -3,6 +3,7 @@ package com.genai.java.spring.config;
 import com.openai.client.OpenAIClient;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openaisdk.OpenAiSdkChatModel;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +13,10 @@ public class AIProviderConfig {
     @Bean("openAIChatClient")
     ChatClient openAIChatClient(OpenAiSdkChatModel openAiSdkChatModel) {
         return ChatClient.builder(openAiSdkChatModel).build();
+    }
+
+    @Bean("vertexAIChatClient")
+    ChatClient vertexAIChatClient(VertexAiGeminiChatModel vertexAiGeminiChatModel) {
+        return ChatClient.builder(vertexAiGeminiChatModel).build();
     }
 }
